71b6adad39a76c4b32efa2173d19a5dbd7ea9e8a
vonas <24623262+vonas@users.noreply.github.com>
2021-01-06 22:33:59 +0100

    Update run.sh

--- run.sh
+++ run.sh
@@ -1,10 +1,7 @@
 #!/bin/bash
 
-IN_DIR=in
-OUT_DIR=out
-
 cd $(dirname $0)
-make IN_DIR=$IN_DIR OUT_DIR=$OUT_DIR convert
+./convert-mp4.sh
 
 echo "Done."
 read -p "Press enter to close this window."
